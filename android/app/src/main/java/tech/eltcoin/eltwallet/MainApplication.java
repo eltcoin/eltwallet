f02bab78b4674ad1eb6dc7f327b41dbaaec3f372
Victor Faramond <victor.faramond@gmail.com>
2018-06-21 19:19:25 +0100

    Update packages / Remove Sentry / Update Gradle

--- android/app/src/main/java/tech/eltcoin/eltwallet/MainApplication.java
+++ android/app/src/main/java/tech/eltcoin/eltwallet/MainApplication.java
@@ -31,14 +31,14 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new ReactNativeConfigPackage(),
-          new SvgPackage(),
-          new LinearGradientPackage(),
-          new RandomBytesPackage(),
-          new RNCameraPackage(),
-          new RNSensitiveInfoPackage(),
-          new ReactNativeDialogsPackage(),
-          new ReactNativeFingerprintScannerPackage()
+            new ReactNativeConfigPackage(),
+            new SvgPackage(),
+            new LinearGradientPackage(),
+            new RandomBytesPackage(),
+            new RNCameraPackage(),
+            new RNSensitiveInfoPackage(),
+            new ReactNativeDialogsPackage(),
+            new ReactNativeFingerprintScannerPackage()
       );
     }
 
--- index.js
+++ index.js
@@ -3,15 +3,10 @@ import { AppRegistry, StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import Config from 'react-native-config';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Sentry } from 'react-native-sentry';
 import './shim';
 import Navigator from './src/navigators';
 import { persistor, store } from './src/config/store';
 import AnalyticsUtils from './src/utils/analytics';
-
-if (process.env.NODE_ENV === 'production') {
-  Sentry.config(Config.SENTRY_API_KEY).install();
-}
 
 const getCurrentRouteName = navigationState => {
   if (!navigationState) {
